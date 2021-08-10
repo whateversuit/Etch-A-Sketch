57aa1403d243721206c39b6a8c59a01e69489cb0
Emil Lindström <lindstromemil86@gmail.com>
2021-08-10 14:22:25 +0200

    added mouseover background change

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 //Select container div.
+
 const container = document.querySelector('.container');
 
 
+let grid;
 function createGrid(rows, columns){
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-columns', columns);
     //Changes CSS to the desired amount of rows and columns.
     for (let i = 0; i < (rows * columns); i++) {
-        let grid = document.createElement('div');
-        grid.innerText = "0"
-        container.appendChild(grid).className = "grid-style";
+        grid = document.createElement('div');
+        grid.addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = "red";})
+        container.appendChild(grid).classList = "grid-style";
     //Creating a loop that loops through the desired grid and creating divs attached to container.
         
     }
@@ -18,3 +21,9 @@ function createGrid(rows, columns){
 
 createGrid(16, 16);
 
+
+// make grid generated stay within the container size. 
+
+// make change background on hover
+
+// create button that 
