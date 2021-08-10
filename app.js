baf3ecb4da1acfdbfd257ff8c4f5a765b32daa29
Emil Lindström <lindstromemil86@gmail.com>
2021-08-10 12:46:47 +0200

    added function that makes a grid

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
+//Select container div.
 const container = document.querySelector('.container');
+
+
+function createGrid(rows, columns){
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-columns', columns);
+    //Changes CSS to the desired amount of rows and columns.
+    for (let i = 0; i < (rows * columns); i++) {
+        let grid = document.createElement('div');
+        grid.innerText = "0"
+        container.appendChild(grid).className = "grid-style";
+    //Creating a loop that loops through the desired grid and creating divs attached to container.
+        
+    }
+    
+}
+
+createGrid(16, 16);
 
