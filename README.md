5432d3949f82905cd811dcc25dbc63ab0118beeb
Emil Lindström <lindstromemil86@gmail.com>
2021-08-12 06:58:37 +0200

    added prompt for grid choises

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Etch-A-Sketch
 
 A javaScript Etch a Sketch Program
+
+future upgrades:
+
+1. Random RBG-color
+2. Will come back later to fick eraser, dont have the knowledge right now.
+3. Add slider for choosing grid size instead of prompt.
--- app.js
+++ app.js
@@ -2,24 +2,45 @@
 const container = document.querySelector('.container');
 const newBtn = document.getElementById('btn-sketch');
 const eraseBtn = document.querySelector('.btn-eraser');
-const newGrid = document.querySelector('.grid-style');
+
+let rows = 16;
+let columns = 16;
+// const chooseGrid = prompt("Choose grid size (1-100): ")
+// columns = chooseGrid;
+// rows = chooseGrid;
 
 newBtn.addEventListener('click', () => {
-    container.innerText = "";
-    createGrid(20, 20);
-    
+    while (container.firstChild){
+        container.removeChild(container.firstChild);
+
+    }
+    const chooseGrid = prompt("Choose grid size (1-100): ")
+    if (chooseGrid > 100 || chooseGrid < 1 ){
+        let chooseGrid = 10;
+        createGrid(chooseGrid);
+        alert('Cannot make grids larger than 100x100 or less than 0!');
+
+    } else {
+    columns = chooseGrid;
+    rows = chooseGrid;
+    createGrid(chooseGrid);   
+    }
 })
+
+
+// Cant fix this right now.
 eraseBtn.addEventListener('click', () => {
     console.log("Clicked");
-    grid.addEventListener('mouseover', (e) => {
-        e.target.style.backgroundColor = "";})
-    
-})
+    {
+    // grid.addEventListener('mouseover', (e) => {
+    // e.target.style.backgroundColor = "";})
+     
+}})
 
 
 
 let grid;
-function createGrid(rows, columns){
+function createGrid(chooseGrid){
     
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-columns', columns);
@@ -29,11 +50,13 @@ function createGrid(rows, columns){
         grid.addEventListener('mouseover', (e) => {
         e.target.style.backgroundColor = "rgba(0, 0, 0, 0.5)";})
         container.appendChild(grid).classList = "grid-style";
-    //Creating a loop that loops through the desired grid and creating divs attached to container.
+    
+        //Creating a loop that loops through the desired grid and creating divs attached to container.
     //Also changes background color on mouseover on each grid item.
     }
     
 }
-createGrid(20, 20)
+createGrid(rows, columns)
 
 // Maybe add random RGB-colors
+
